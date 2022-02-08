b05cecf45aa51b7e807336269ecc1cc9931ecc4a
Bryan Dube <bdube@nozgames.com>
2022-02-08 04:53:13 -0600

    Actor Spawner

---
+++ Assets/Scripts/Game/ActorSpawner.cs
@@ -0,0 +1,19 @@
+using Unity.Netcode;
+using UnityEngine;
+
+namespace NoZ.Zisle
+{
+    public class ActorSpawner : MonoBehaviour
+    {
+        [SerializeField] private ActorDefinition[] _actorDefinitions = null;
+
+        private void Start()
+        {
+            if(_actorDefinitions != null)
+            {
+                var def = _actorDefinitions[Random.Range(0, _actorDefinitions.Length)];
+                Instantiate(def.Prefab, transform.position, transform.rotation).GetComponent<NetworkObject>().Spawn();
+            }
+        }
+    }
+}
